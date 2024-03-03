5ad25968b5990f0f6b1c5a0a633b36f0764666a1
ahmed-said7 <engahmedsaid125@gmail.com>
2024-03-03 23:58:30 +0200

    1st commit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { catchExceptionsFilter } from './filters/base.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const {httpAdapter}=app.get(HttpAdapterHost)
   app.useGlobalFilters(new catchExceptionsFilter(httpAdapter))
+  app.useGlobalPipes(new ValidationPipe({whitelist: true}))
   await app.listen(4000);
 }
 bootstrap();
